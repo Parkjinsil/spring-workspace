df8710c71c22225202324388be5c8a9786c5b1d8
박진실 <wlstlf971017@naver.com>
2023-09-06 16:01:49 +0900

    0906feat

--- 07_RestfulAPI/src/main/java/com/kh/api/dao/PhoneDAO.java
+++ 07_RestfulAPI/src/main/java/com/kh/api/dao/PhoneDAO.java
@@ -8,8 +8,9 @@ import com.kh.api.model.UserInfo;
 public interface PhoneDAO { // impl -> PhoneDAOImpl
 
 	int insert(Phone phone);
-	int delete(List<String> list);
-	Phone select(Phone phone);
+	int delete(String num);
+	int update(Phone phone);
+	Phone select(String num);
 	List<Phone> select();
 	UserInfo select(UserInfo user);
 	
